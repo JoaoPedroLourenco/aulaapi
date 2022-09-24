9c5e1c7c8585ee4150a9a0f40cf912afdca76f9e
Joao Pedro <joao.santos1776@etec.sp.gov.br>
2022-09-24 11:39:39 -0300

    ednaldomoreira

---
+++ lib/view/Home/homeView.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:money_search/data/MoneyController.dart';
+import 'package:money_search/model/MoneyModel.dart';
+import 'package:money_search/model/listPersonModel.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+/// instancia do modelo para receber as informações
+List<ListPersonModel> model = [];
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Lista de pessoas'),
+          centerTitle: true,
+          backgroundColor: Colors.lightGreen,
+        ),
+        body: FutureBuilder<List<ListPersonModel>>(
+          future: MoneyController().getListPerson(),
+          builder: (context, snapshot) {
+            /// validação de carregamento da conexão
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            /// validação de erro
+            if (snapshot.error == true) {
+              return SizedBox(
+                height: 300,
+                child: Text("Vazio"),
+              );
+            }
+//  List<ListPersonModel> model = [];
+            /// passando informações para o modelo criado
+            /// 
+            model = snapshot.data ?? model;
+            model.add(ListPersonModel(avatar: "https://i1.sndcdn.com/artworks-iEBoUoJhkodCKbIl-zLXabQ-t500x500.jpg", id: "777", name: "Ednaldo Moreira"));
+            model.sort(
+              (a, b) => a.name!.compareTo(b.name!),
+            );
+            model.removeWhere((pessoa) => pessoa.id=="64");
+            model.removeWhere((pessoa) => pessoa.name=="Miss Kim Keebler");
+            model.forEach((element) {
+              if (element.id=="9"){
+                element.avatar="";
+              }
+            });
+          
+            return ListView.builder(
+                itemCount: model.length,
+                itemBuilder: (context, index) {
+                  ListPersonModel item = model[index];
+                  return ListTile(
+                    leading: Image.network(
+                       errorBuilder: (context, error, stackTrace)
+                       {
+                        return Container();
+                       }, item.avatar ?? ""),
+                    title: Text(item.name ?? ""),
+                    trailing: Text(item.id ?? ""),
+                  );
+                });
+            // ListView.builder(
+            //   shrinkWrap: true,
+            //   // physics: NeverScrollableScrollPhysics(),
+            //   itemCount: model.length,
+            //   itemBuilder: (BuildContext context, int index) {
+            //     ListPersonModel item = model[index];
+            //     // tap(ListPersonModel item) {
+            //     //   Navigator.push(
+            //     //       context,
+            //     //       MaterialPageRoute(
+            //     //           builder: (context) => Person(
+            //     //                 item: item,
+            //     //               )));
+            //     // }
+
+            //     return GestureDetector(
+            //       // onTap: (() => tap(item)),
+            //       child: ListTile(
+            //         leading: Image.network(item.avatar ?? ""),
+            //         title: Text(item.name ?? ""),
+            //         trailing: Text(item.id ?? ""),
+            //       ),
+            //     );
+            //   },
+            // );
+          },
+        ));
+  }
+
+  Future<Null> refresh() async {
+    setState(() {});
+  }
+}
